24ff28af65206f0638d3716136ef63320b6c73b3
kahnn286 <hieuhoang2861980@gmail.com>
2025-11-13 12:21:52 +0700

    product

--- app/user/login/page.jsx
+++ app/user/login/page.jsx
@@ -92,7 +92,7 @@ export default function UserLogin() {
 
           <CardFooter className="flex flex-col space-y-4">
             <div className="text-sm text-center text-muted-foreground">
-              Don't have an account?{' '}
+              Don&apos;t have an account?{' '}
               <Link href="/user/register" className="text-primary hover:underline font-medium">
                 Sign up
               </Link>
